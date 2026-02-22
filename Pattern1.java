20ce22a8b3026238c72fcd868bf374a57f30efda
venu <venugopalgottimukkala99@gmail.com>
2026-02-22 11:54:24 +0530

    changed content in Pattern in Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -6,7 +6,7 @@ public class Pattern1 {
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                System.out.print("* ");
+                System.out.print("*  ");
             }
             System.out.println(" ");
         }
