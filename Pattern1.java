bbb89a3ef66400b30d142287e22a8ee0cee605c1
venu <venugopalgottimukkala99@gmail.com>
2026-02-23 20:49:57 +0530

    Code is now modified to upper traingular matrix

--- Pattern1.java
+++ Pattern1.java
@@ -4,12 +4,12 @@ public class Pattern1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*  ");
             }
             System.out.println(" ");
-        }
+        }   
     }
 }
 
