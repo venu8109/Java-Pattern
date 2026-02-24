90e6f7bf9eed7faba03942e6e1ab5e4a5a501a9f
venu <venugopalgottimukkala99@gmail.com>
2026-02-24 09:38:27 +0530

    A little modification

--- Pattern1.java
+++ Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");
         int n = sc.nextInt();
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*  ");
             }
